ec574c6eb454b60f6ada0b9bfdf56af8782a0e39
Jeferson SIlva <34014937+silvajeferson82@users.noreply.github.com>
2021-05-09 00:12:17 -0300

    Utilização de useContext

--- src/Aula2/App.js
+++ src/Aula2/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import "../styles.css";
+
+const ThemeContext = createContext();
+
+function Button() {
+  const theme = useContext(ThemeContext);
+
+  return <button>{theme.mode}</button>;
+}
 
 export default function App() {
   return (
     <div className="App">
       <h1>UseContext</h1>
+      <ThemeContext.Provider value={{ mode: "white" }}>
+        <Button />
+      </ThemeContext.Provider>
+      <br />
+      <ThemeContext.Provider value={{ mode: "dark" }}>
+        <Button />
+      </ThemeContext.Provider>
     </div>
   );
 }
